59251f921b0197f211c9fc9262ab6ad030861e1c
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-06 11:25:17 +0530

    Welcome to employee wage computation

---
+++ EmpWageComputation.java
@@ -0,0 +1,15 @@
+/**
+ *  Compilation:  javac -d bin EmpWageComputation.java
+ *  Purpose: Compute the Employee Wages.
+ *  @author  Dhiraj
+ *  @version 1.0
+ *  @since   06-06-2021
+ * */
+
+package bridgelabz;
+
+public class EmpWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
