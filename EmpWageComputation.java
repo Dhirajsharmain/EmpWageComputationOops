7464b5d21172ab4bb73d36b8a209b1217d673924
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-06 21:24:39 +0530

    Added: Check Employee is Present or Absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,15 +1,42 @@
 /**
- *  Compilation:  javac -d bin EmpWageComputation.java
- *  Purpose: Compute the Employee Wages.
- *  @author  Dhiraj
- *  @version 1.0
- *  @since   06-06-2021
- * */
+ * Compilation:  javac -d bin EmpWageComputation.java
+ * Purpose: Compute the Employee Wages.
+ *
+ * @author Dhiraj
+ * @version 1.0
+ * @since 06-06-2021
+ */
 
 package bridgelabz;
 
+import java.util.Random;
+
+class EmployeeWage {
+    int isPresent = 1;
+
+    /**
+     * Method for taking attendance of employee.
+     */
+    public void attendance() {
+        Random random = new Random();
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck == isPresent) {
+            System.out.println("Employee is Present ");
+        } else {
+            System.out.println("Employee is Absent ");
+        }
+    }
+}
+
 public class EmpWageComputation {
+
+    /**
+     * This is main method, This is a starting point of EmpWageComputation program.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        EmployeeWage EmployeeWageObject = new EmployeeWage();
+        EmployeeWageObject.attendance();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Emp_Wage_Computation_Day8
+
+**UC1-EmpAttendance**
+
+**_Description_** : Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check
