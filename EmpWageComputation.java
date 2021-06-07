03e53cb7d7d537ffc5e243697ed349c796ec07f1
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-07 14:35:21 +0530

    Welcome to Employee Wage Computation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -9,52 +9,14 @@
 
 package bridgelabz;
 
-import java.util.Random;
-
-class EmployeeWage {
-    public static final int EMP_RATE_PER_HOUR = 20;
-    int isFullTime = 1;
-    int isPartTime = 2;
-    int empHrs = 0;
-    int empWage;
-
-    /**
-     * Method for taking attendance of employee.
-     */
-    public void attendance() {
-        Random random = new Random();
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if (empCheck == isFullTime) {
-            System.out.println("Employee is Present Full Time");
-            empHrs = 8;
-        } else if (empCheck == isPartTime) {
-            System.out.println("Employee is Present Part Time ");
-            empHrs = 4;
-        } else {
-            System.out.println("Employee is Absent ");
-        }
-    }
-
-    /**
-     * Method for calculating daily wage for employee.
-     */
-    public void calculatingWage() {
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage is : " + empWage);
-    }
-}
 
 public class EmpWageComputation {
 
     /**
      * This is main method, This is a starting point of EmpWageComputation program.
-     *
      * @param args
      */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        EmployeeWage EmployeeWageObject = new EmployeeWage();
-        EmployeeWageObject.attendance();
-        EmployeeWageObject.calculatingWage();
     }
 }
--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # Emp_Wage_Computation_Day8
-
-**UC1-EmpAttendance**
-
-**_Description_** : Check Employee is Present or Absent - Use ((RANDOM)) for Attendance Check
-
-**UC2-CalculatingDailyWage**
-
-**_Description_** : Calculate Daily Employee Wage - Assume wage per hour is 20 - Assume full day hour is 8.
-
-**UC3-PartTimeEmployee**
-
-**_Description_** : Add Part time Employee & Wage - Assume Part time Hour is 8
